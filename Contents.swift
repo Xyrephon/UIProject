efea0ba33ebe270f49672a2ebaef9a77f7f77ae6
Xyrephon <jtuc1613@740s-pl-115.local>
2016-09-21 08:15:19 -0600

    Created 3 views in the playground.

---
+++ Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var DynamicView = UIView(frame: CGRectMake(100, 200, 100, 100))
+DynamicView.backgroundColor=UIColor.greenColor()
+DynamicView.layer.cornerRadius=30
+DynamicView.layer.borderWidth=2
+DynamicView
+
+var View2 = UIView(frame: CGRectMake(200, 300, 450, 150))
+View2.backgroundColor=UIColor.blueColor()
+View2.layer.cornerRadius=250
+View2.layer.borderWidth=25
+View2
+
+var ViewPractice3 = UIView(frame: CGRectMake(20, 20, 200, 200))
+ViewPractice3.backgroundColor=UIColor.purpleColor()
+ViewPractice3.layer.cornerRadius=200
+ViewPractice3.layer.borderWidth=10
+ViewPractice3.alpha = CGFloat(0.8)
+
+
+
+
